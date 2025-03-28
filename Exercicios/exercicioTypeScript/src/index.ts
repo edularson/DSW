68ee590ae17228686bc0d5e1f6914b6a0c433006
Eduardo Larson <138933830+edularson@users.noreply.github.com>
2025-03-28 11:46:46 -0300

    Update index.ts

--- Exercicios/exercicioTypeScript/src/index.ts
+++ Exercicios/exercicioTypeScript/src/index.ts
@@ -17,11 +17,11 @@ const cliente2 = new Cliente("Thiago Ceron", 12332112312, 10022005, "Masculino",
 
 const produto1 = new Produto(1, "Camiseta Flamengo", 450);
 const produto2 = new Produto(2, "Chuteira Nike Mercurial", 600);
-const produto3 = new Produto(1, "Camiseta Corinthians", 400);
-const produto4 = new Produto(2, "Boné Corinthians", 100);
+const produto3 = new Produto(3, "Camiseta Corinthians", 400);
+const produto4 = new Produto(4, "Boné Corinthians", 100);
 
-const venda1 = new Venda(1001, 28032025, cliente1, [produto1, produto2]);
-const venda2 = new Venda(1001, 28032025, cliente2, [produto3, produto4]);
+const venda1 = new Venda(1001, 11111111, cliente1, [produto1, produto2]);
+const venda2 = new Venda(1002, 22222222, cliente2, [produto3, produto4]);
 
 console.log("Cliente:", cliente1);
 console.log("Endereço:", cliente1.endereco);
